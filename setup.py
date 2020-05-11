470517a3e1cbd49b15ce7171eb9886dafdd25fe6
Andrea Giovanni Nuzzolese <andrea.nuzzolese@gmail.com>
2020-05-11 19:35:04 +0200

    Upgrade of Jinja2 dependency to version 2.10.1 in order to fix the vulnerability related to version 2.10.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 install_requires=[
    'lark-parser>=0.8.5,<=0.8.5',
    'pandas>=0.24.1,<=0.24.1',
-   'Jinja2>=2.10,<=2.10',
+   'Jinja2>=2.10.1,<=2.10.1',
    'rdflib>=4.2.2,<=4.2.2',
    'rdflib>=4.2.2,<=4.2.2',
    'Unidecode>=1.1.1,<=1.1.1'
