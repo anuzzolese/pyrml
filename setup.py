2fdde948e12555ce4f3e5052afd65d86806a3376
Andrea Giovanni Nuzzolese <andrea.nuzzolese@gmail.com>
2020-05-11 19:00:09 +0200

    Deps.
    Added dependencies management into setup.py.

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from distutils.core import setup
 
-setup(name='pyrml', version='1.0.0')
+install_requires=[
+   'lark-parser>=0.8.5,<=0.8.5',
+   'pandas>=0.24.1,<=0.24.1',
+   'Jinja2>=2.10,<=2.10',
+   'rdflib>=4.2.2,<=4.2.2',
+   'rdflib>=4.2.2,<=4.2.2',
+   'Unidecode>=1.1.1,<=1.1.1'
+]
+
+setup(name='pyrml', version='1.0.0', install_requires=install_requires)
+
