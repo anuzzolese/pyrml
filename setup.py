63fbebc28f8c8229b247cec625bed712bc289790
Andrea Giovanni Nuzzolese <andrea.nuzzolese@gmail.com>
2023-04-05 16:44:42 +0200

    Added SPARQLWrapper among the requirements into setup.py.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ install_requires=[
    'pandas>=1.5.1,<=1.5.1',
    'Jinja2>=3.1.2,<=3.1.2',
    'rdflib>=6.2.0,<=6.2.0',
+   'SPARQLWrapper>=2.0.0,<=2.0.0',
    #'Unidecode>=1.3.6,<=1.3.6',
    'Flask>=2.2.2,<=2.2.2',
    'jsonpath-ng>=1.5.3,<=1.5.3',
